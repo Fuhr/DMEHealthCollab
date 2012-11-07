df62f33417c197f7fb798e6d872f8b41b019a5da
Sfuhr <soren.fuhr@gmail.com>
2012-11-08 00:56:15 +0100

    Created test canvas

--- app.js
+++ app.js
@@ -4,21 +4,21 @@
  */
 
 var express = require('express')
-  // , routes = require('./app/server/routes')
-  , sio = require('socket.io')
+  , io = require('socket.io')
   , http = require('http')
   , path = require('path');
 
+/* Initialize app and config */
 var app = express();
 
 app.root = __dirname;
 require('./app/config')(app, express);
 require('./app/server/router')(app);
 
-// app.get('/test', routes.test);
-// app.get('/', routes.index);
 
-
-http.createServer(app).listen(app.get('port'), function(){
+/* Create server */
+var server = http.createServer(app);
+io.listen(server);
+server.listen(app.get('port'), function(){
   console.log("Express server listening on port " + app.get('port'));
 });
