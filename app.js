256d14e2c14668a8b67c74a974e9d501fb32edbf
Soeren <soren.fuhr@gmail.com>
2012-11-13 15:47:17 +0100

    Clean-up of app.js - moved socket code to router.js

--- app.js
+++ app.js
@@ -1,35 +1,26 @@
 
 /**
- * Module dependencies.
+ * Module dependencies,
+ * initialize app, and config
  */
 
 var express = require('express')
-  
-
-/* Initialize app and config */
 var app = express()
-    , io = require('socket.io')
-    , http = require('http')
+    , server = require('http').createServer(app)
+    , io = require('socket.io').listen(server)
     , path = require('path');
 
 app.root = __dirname;
 require('./app/config')(app, express);
-require('./app/server/router')(app);
+require('./app/server/router')(app, io);
 
-
-/* Create server */
-var server = http.createServer(app);
-io = io.listen(server);
 server.listen(app.get('port'), function(){
   console.log("Express server listening on port " + app.get('port'));
 });
 
-// TESTING PURPOSES ONLY - TO BE DELETED OR MOVED
-io.sockets.on('connection', function (socket) {
-    socket.on('rect', function (data) {
-        io.sockets.emit('rectSend', data);
-   });
-});
+
+
+
 
 
 
--- app/server/router.js
+++ app/server/router.js
@@ -1,6 +1,6 @@
-module.exports = function(app) {
+module.exports = function(app, io) {
 
-
+    /* Page routing*/
 	app.get('/test', function(req, res){
 	    res.render('test', {});
 	});
@@ -9,4 +9,10 @@ module.exports = function(app) {
 	    res.render('index', {});
 	});	
 
+    /* Socket handlers */
+    io.sockets.on('connection', function (socket) {
+        socket.on('rect', function (data) {
+            io.sockets.emit('rectSend', data);
+       });
+    });
 };
