3ef8b0123efe333234501a05bb3830724c3fba31
Soeren <soren.fuhr@gmail.com>
2012-11-13 16:51:30 +0100

    Colors of rects are now random upon drawing (testCanvas)

--- app/public/js/testCanvas.js
+++ app/public/js/testCanvas.js
@@ -11,11 +11,18 @@ $(document).ready(function() {
     stage.add(layer);
     var canvas = stage.getContainer();     
     var xo, yo, xe, ye;
-         
-     canvas.addEventListener('mousedown' , function(evt) {
+    
+    canvas.addEventListener('mousedown' , function(evt) {
          // console.log("x: "+ evt.layerX + "\n" + "y: " + evt.layerY);
          xo = evt.layerX;
          yo = evt.layerY;
+     });
+     canvas.addEventListener('mouseup', function(evt) {
+         // console.log("x: "+ evt.layerX + "\n" + "y: " + evt.layerY);
+         xe = evt.layerX;
+         ye = evt.layerY;
+
+         socket.emit('rect', {x0: xo, y0: yo, x1: xe, y1: ye});
      });
          
      socket.on('connect', function () {
@@ -25,30 +32,35 @@ $(document).ready(function() {
          console.log(hello);
        });
        
-       
-        canvas.addEventListener('mouseup', function(evt) {
-            // console.log("x: "+ evt.layerX + "\n" + "y: " + evt.layerY);
-            xe = evt.layerX;
-            ye = evt.layerY;
-
-            socket.emit('rect', {x0: xo, y0: yo, x1: xe, y1: ye});
-        });  
-     
-        socket.on('rectSend', function(data) {
-        
-            console.log("Received - x0: " + data.x0);
-            layer.add(new Kinetic.Rect({
-                       x: data.x0,
-                       y: data.y0,
-                       width: data.x1-data.x0,
-                       height: data.y1-data.y0,
-                       fill: 'red',
-                       stroke: 'black',
-                       strokeWidth: 1,
-                       draggable: true
-                   })
-                   );
-                   layer.draw();
+       socket.on('rectSend', function(data) {
+           console.log("Received - x0: " + data.x0);
+           drawTestRect(layer, data);
         });  
     });
  });
+
+ function rndColor() {
+     function c() {
+         return ('0' + Math.floor(Math.random() * 256).toString(16)).substr(-2);
+     }
+     return '#' + c() + c() + c();
+ }
+
+ 
+function drawTestRect(layer, data) {
+    
+    var color = rndColor();
+        
+    layer.add(new Kinetic.Rect({
+           x: data.x0,
+           y: data.y0,
+           width: data.x1-data.x0,
+           height: data.y1-data.y0,
+           fill: color,
+           stroke: 'black',
+           strokeWidth: 1,
+           draggable: true
+        }));
+    layer.draw();
+}
+
--- app/server/router.js
+++ app/server/router.js
@@ -9,7 +9,11 @@ module.exports = function(app, io) {
 	    res.render('index', {});
 	});	
 
-    /* Socket handlers */
+    /* Socket handlers
+    * Any functions related to socket handlers should have its own modules!
+    * See this example: http://erickrdch.com/2012/05/chat-application-with-node-js-and-socket-io.html
+    * for module exports see: http://www.hacksparrow.com/node-js-exports-vs-module-exports.html
+    */
     io.sockets.on('connection', function (socket) {
         socket.on('rect', function (data) {
             io.sockets.emit('rectSend', data);
