01313fa6e03a1a414c79684cd460f22a1fbca9b7
Soeren <soren.fuhr@gmail.com>
2012-11-18 15:22:31 +0100

    Organized Canvas code. Made New controller and util scripts

---
+++ app/public/js/controllers/canvasController.js
@@ -0,0 +1,87 @@
+function canvasController(parentDiv, socket) {
+    var cu = new canvasUtils();
+    var coords = {"x0":"","x1":"","y0":"","y1":""};
+
+
+    var stage = cu.createStage(parentDiv, '700', '525');
+    var layer = cu.createLayer(stage);
+    var canvas = stage.getContainer();         
+
+
+
+    /* Event handlers */ 
+    canvas.addEventListener('mousedown' , function(evt) {
+        var pos = getMousePositionOnCanvas(evt);         
+        coords.x0 = pos.x;
+        coords.y0 = pos.y;
+    });
+
+    canvas.addEventListener('mouseup', function(evt) {
+        var pos = getMousePositionOnCanvas(evt);
+        coords.x1 = pos.x;
+        coords.y1 = pos.y;
+
+        sendRectToServer(socket, coords);
+    });
+
+    canvas.addEventListener('touchstart', function(evt) {
+        var pos = getTouchPositionOnCanvas(evt);
+        coords.x0 = pos.x;
+        coords.y0 = pos.y;
+    });
+
+    canvas.addEventListener('touchend', function(evt) {
+        var pos = getTouchPositionOnCanvas(evt);
+        coords.x1 = pos.x;
+        coords.y1 = pos.y;
+
+        sendRectToServer(socket, coords);
+    });
+
+
+    /* Socket handlers */
+    socket.on('connect', function () {
+        socket.on('rectSend', function(data) {
+            cu.drawTestRect(layer, data);
+        });  
+    });
+
+     
+    getMousePositionOnCanvas = function(evt) {
+        var result = {'x': '', 'y': ''};
+        result.x = evt.layerX;
+        result.y = evt.layerY;
+
+        return result;
+    }
+
+    getTouchPositionOnCanvas = function(evt) {
+        var result = {'x': '', 'y': ''};
+        var touchPosition = stage.getTouchPosition();
+        result.x = touchPosition.x;
+        result.y = touchPosition.y;
+
+        return result;
+    }
+
+    sendRectToServer = function(socket, coords) {
+        dx = coords.x1 - coords.x0;
+        dy = coords.y1 - coords.y0;
+
+        console.log("dx: " + dx + " dy: " + dy);
+
+        // TODO: This statement is bugged - doesn't take negative values into account
+        /* Exits function if draw distance is too small */
+
+        // if(dx <= 25 && dy <= 25 || dx = 25 || dy <= 25) {
+        //         return
+        // }
+
+        /* Send shape information to server, thus pushing to other clients */ 
+        socket.emit('rect', {
+            coords: coords,
+            dx: dx,
+            dy: dy,
+            color: cu.rndColor()});
+    }
+}
---
+++ app/public/js/utils/canvasUtils.js
@@ -0,0 +1,41 @@
+function canvasUtils() {
+    
+    this.createStage = function(parent, width, height) {
+        var stage = new Kinetic.Stage({
+            container: parent,
+            width: width, //700
+            height: height //525
+        });
+        
+        return stage;
+    }
+    
+    this.createLayer = function(stage) {
+        var layer = new Kinetic.Layer();
+        stage.add(layer);
+        
+        return layer;
+    }
+    
+    this.drawTestRect = function(layer, data) {
+        layer.add(new Kinetic.Rect({
+               x: data.coords.x0,
+               y: data.coords.y0,
+               width: data.dx,
+               height: data.dy,
+               fill: data.color,
+               stroke: 'black',
+               strokeWidth: 1,
+               draggable: true
+            }));
+        layer.draw();
+    }
+    
+    // Test function. Should be deleted in the future
+    this.rndColor = function() {
+         function c() {
+             return ('0' + Math.floor(Math.random() * 256).toString(16)).substr(-2);
+         }
+         return '#' + c() + c() + c();
+    }
+}
