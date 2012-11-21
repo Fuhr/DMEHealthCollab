235ca94337d7bbf1a679bc9a9f2573d393933952
Soeren <soren.fuhr@gmail.com>
2012-11-21 17:59:20 +0100

    Cleaned up up canvasController (fixed duplicate code)

--- app/public/js/controllers/canvasController.js
+++ app/public/js/controllers/canvasController.js
@@ -23,7 +23,6 @@ function canvasController(parentDiv, socket) {
         mouseDown = true;
         
         var pos = getMousePositionOnCanvas(evt);       
-        //indicatorShape = cu.drawIndicatorRect(layer, pos);
         indicatorShape = indicFunctions[shape.form](layer, pos);
         
         shape.x0 = pos.x;
@@ -32,63 +31,24 @@ function canvasController(parentDiv, socket) {
         indicatorData.y0 = pos.y;
         indicatorData.x1 = pos.x;
         indicatorData.y1 = pos.y;
-        
     });
     
     canvas.addEventListener('mousemove', function(evt){
         if (mouseDown === true) {
-            
-            
             var pos = getMousePositionOnCanvas(evt);       
-            if (Math.abs(indicatorData.x1-pos.x) > 10 || Math.abs(indicatorData.y1-pos.y) > 10) {
-                indicatorData.x1 = pos.x;
-                indicatorData.y1 = pos.y;
-                
-                indicatorData.calcDelta();
-                
-                if (shape.form === 'rect') {
-                    indicatorShape.attrs.width = indicatorData.dx;
-                    indicatorShape.attrs.height = indicatorData.dy;
-                    
-                } 
-                else if (shape.form === 'ellipse') {
-                    
-                    indicatorData.calcEllipseOrigo();
-                    indicatorShape.attrs.x = indicatorData.ellipseOrigo.x;
-                    indicatorShape.attrs.y = indicatorData.ellipseOrigo.y;
-                                        
-                    indicatorShape.attrs.radius.x = Math.abs(indicatorData.dx / 2);
-                    indicatorShape.attrs.radius.y = Math.abs(indicatorData.dy / 2);
-
-                }
-                else if (shape.form === 'circle') {
-                    
-                    indicatorData.calcEllipseOrigo();
-                    indicatorShape.attrs.x = indicatorData.ellipseOrigo.x;
-                    indicatorShape.attrs.y = indicatorData.ellipseOrigo.y;
-                                        
-                    indicatorShape.attrs.radius = Math.abs(Math.sqrt(dx*dx + dy*dy)/2);
-                }
-                
-                layer.draw();      
-                                
-            }    
+            
+            updateIndicatorShape(pos);  
         }
-        
-         
     });
-    
-
 
     canvas.addEventListener('mouseup', function(evt) {  
         if (isDraggable()) return;
                 
         mouseDown = false;
-        indicatorShape.remove();
         var pos = getMousePositionOnCanvas(evt);
         shape.x1 = pos.x;
         shape.y1 = pos.y;
-
+        
         sendShapeToServer(socket, shape);
     });
 
@@ -96,7 +56,6 @@ function canvasController(parentDiv, socket) {
         if (isDraggable()) return;      
         
         var pos = getTouchPositionOnCanvas(evt);       
-        //indicatorShape = cu.drawIndicatorRect(layer, pos);
         indicatorShape = indicFunctions[shape.form](layer, pos);
         
         shape.x0 = pos.x;
@@ -110,51 +69,17 @@ function canvasController(parentDiv, socket) {
     canvas.addEventListener('touchmove', function(evt){
 
         var pos = getTouchPositionOnCanvas(evt);       
-        if (Math.abs(indicatorData.x1-pos.x) > 10 || Math.abs(indicatorData.y1-pos.y) > 10) {
-            indicatorData.x1 = pos.x;
-            indicatorData.y1 = pos.y;
-            
-            indicatorData.calcDelta();
-            
-            if (shape.form === 'rect') {
-                indicatorShape.attrs.width = indicatorData.dx;
-                indicatorShape.attrs.height = indicatorData.dy;
-                
-            } 
-            else if (shape.form === 'ellipse') {
-                
-                indicatorData.calcEllipseOrigo();
-                indicatorShape.attrs.x = indicatorData.ellipseOrigo.x;
-                indicatorShape.attrs.y = indicatorData.ellipseOrigo.y;
-                                    
-                indicatorShape.attrs.radius.x = Math.abs(indicatorData.dx / 2);
-                indicatorShape.attrs.radius.y = Math.abs(indicatorData.dy / 2);
-
-            }
-            else if (shape.form === 'circle') {
-                
-                indicatorData.calcEllipseOrigo();
-                indicatorShape.attrs.x = indicatorData.ellipseOrigo.x;
-                indicatorShape.attrs.y = indicatorData.ellipseOrigo.y;
-                                    
-                indicatorShape.attrs.radius = Math.abs(indicatorData.dx / 2);
-            }
-            
-            layer.draw();      
-                            
-        }        
+        updateIndicatorShape(pos);
     });
     
     canvas.addEventListener('touchend', function(evt) {
         if (isDraggable()) return;
-
-        indicatorShape.remove();
+        
         var pos = getTouchPositionOnCanvas(evt);
         shape.x1 = pos.x;
         shape.y1 = pos.y;
 
         sendShapeToServer(socket, shape);
-
     });
     
     /* Socket handlers */
@@ -162,19 +87,16 @@ function canvasController(parentDiv, socket) {
         
         socket.on('clientId', function(data) {
             _clientId = data;
-        })  
+        });
         socket.on('drawShape', function(data) {
             
             var node = drawFunctions[data.form](layer, data);
             
-            // console.log(node);
 			if (_draggable) {
 			    setDraggable(_draggable);
 			}
 			
 			setUpNodeHandlers(node,socket);
-			
-            // console.log('Layer: ' + JSON.stringify(layer.getChildren(), null, 4)); 
         });
 		socket.on('shapeMoved', function(data) {
 			cu.moveNode(layer, data);
@@ -187,7 +109,6 @@ function canvasController(parentDiv, socket) {
     this.setActiveShape = function (type) {
           shape.form = type;
     };
-    
     
     setDraggable = function (state) {
         _draggable = state;
@@ -240,23 +161,60 @@ function canvasController(parentDiv, socket) {
 		});
 	};
 	
+	updateIndicatorShape = function(pos) {
+	    if (Math.abs(indicatorData.x1-pos.x) > 10 || Math.abs(indicatorData.y1-pos.y) > 10) {
+            indicatorData.x1 = pos.x;
+            indicatorData.y1 = pos.y;
+            
+            indicatorData.calcDelta();
+            
+            if (shape.form === 'rect') {
+                indicatorShape.attrs.width = indicatorData.dx;
+                indicatorShape.attrs.height = indicatorData.dy;
+                
+            } 
+            else if (shape.form === 'ellipse') {
+                
+                indicatorData.calcEllipseOrigo();
+                indicatorShape.attrs.x = indicatorData.ellipseOrigo.x;
+                indicatorShape.attrs.y = indicatorData.ellipseOrigo.y;
+                                    
+                indicatorShape.attrs.radius.x = Math.abs(indicatorData.dx / 2);
+                indicatorShape.attrs.radius.y = Math.abs(indicatorData.dy / 2);
+
+            }
+            else if (shape.form === 'circle') {
+                
+                indicatorData.calcEllipseOrigo();
+                indicatorShape.attrs.x = indicatorData.ellipseOrigo.x;
+                indicatorShape.attrs.y = indicatorData.ellipseOrigo.y;
+                                    
+                indicatorShape.attrs.radius = Math.abs(indicatorData.dx / 2);
+            }
+            layer.draw();                      
+        }
+	};
+	
 	sendShapeToServer = function(socket, shape) {
 	    shape.id = _clientId + shapeNumber;
         shapeNumber++;
-		
-		shape.calcDelta();
-		console.log(shape);
+    	shape.calcDelta();
 	    shape.color = cu.rndColor();
+		
 		if (shape.form === 'ellipse' || shape.form === 'circle') {
 		    shape.calcEllipseOrigo();
 	    }
 		
 		adx = Math.abs(shape.dx);
 		ady = Math.abs(shape.dy);
+		
 		if(adx <= 25 && ady <= 25 || adx <= 25 || ady <= 25) {
-            indicatorShape.remove();
             return;
         }
+        
+        indicatorShape.remove();
+        layer.draw();
+        
         socket.emit('shapeDrawn', shape);  
 	};
 };
