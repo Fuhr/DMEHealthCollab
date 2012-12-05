05075957b829db428ca9b72208d7056a38b172a6
Soeren <soren.fuhr@gmail.com>
2012-12-05 13:10:08 +0100

    Added chat button and fixed input event bug

--- app/public/js/controllers/chatController.js
+++ app/public/js/controllers/chatController.js
@@ -26,12 +26,27 @@ function chatController(outputSelector, inputSelector, socket){
         });
     });
     
-    $(inputSelector).on('change',function(event){
-        var value = $(this).val();
+    $(inputSelector).on('keypress',function(e){
+        var code = (e.keyCode ? e.keyCode : e.which);
+        if(code == 13){
+            sendToChat(this);
+        } else {
+            return true;
+        }
+    });
+    
+    $('#chat-input-button').on('click', function(event){
+        sendToChat(inputSelector)
+    });
+    
+    
+    sendToChat = function (selector) {
+        var value = $(selector).val();        
         socket.emit('chatToServer', value);
-        $(this).val("");
+        $(selector).val("");
         
         var height = $('#chat-output')[0].scrollHeight;
         $('#chat-output').scrollTop(height);
-    });
+
+    }
 };
